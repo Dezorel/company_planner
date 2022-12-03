2a9f6d0642375ab24234bc5cd35353aabdf8e1bc
Leonid <scorpion23997@gmail.com>
2022-12-03 21:59:48 +0200

    create method get cabinet by id

--- backend/core/services/cabinet/app/models/CabinetModel.go
+++ backend/core/services/cabinet/app/models/CabinetModel.go
@@ -46,7 +46,42 @@ func GetCabinetsByCompany(companyName string) ([]Cabinet, error) {
 }
 
 func GetCabinetInfoById(cabinetId string) (Cabinet, error) {
-	return Cabinet{Id: cabinetId}, nil
+	db := DBConnect()
+
+	defer db.Close()
+
+	query := "SELECT cab.number, cab.size_id, cab.specifications, comp.company_name " +
+		"FROM Cabinets cab " +
+		"INNER JOIN Companies comp ON cab.company_id = comp.id " +
+		"WHERE cab.id = '" + cabinetId + "'"
+
+	selectResult, err := DBQueryMultiRow(db, query)
+
+	if err != nil {
+		return Cabinet{}, errors.New("Cabinets not found!")
+	}
+
+	var answerCabinet Cabinet
+
+	for selectResult.Next() {
+
+		var sizeId int
+		var number, specification, companyName string
+
+		selectResult.Scan(&number, &sizeId, &specification, &companyName)
+
+		answerCabinet = Cabinet{
+			Id:       cabinetId,
+			Number:   number,
+			Size:     strconv.Itoa(sizeId),
+			Property: specification,
+			Company:  companyName,
+		}
+	}
+
+	selectResult.Close()
+
+	return answerCabinet, nil
 }
 
 func CreateCabinet(companyName, cabinetNumber, cabinetSize, cabinetPropreties string) (Cabinet, error) {

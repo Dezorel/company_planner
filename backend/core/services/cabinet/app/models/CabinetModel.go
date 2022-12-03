af72aee34fb61572bd1d24607cd8b02d3d321a74
Leonid <scorpion23997@gmail.com>
2022-12-03 10:38:11 +0200

    update get cabinets by company name method

--- backend/core/services/cabinet/app/models/CabinetModel.go
+++ backend/core/services/cabinet/app/models/CabinetModel.go
@@ -1,7 +1,51 @@
 package models
 
-func GetCabinetsByCompany(companyName string) (Cabinet, error) {
-	return Cabinet{Company: companyName}, nil
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+func GetCabinetsByCompany(companyName string) ([]Cabinet, error) {
+
+	db := DBConnect()
+
+	defer db.Close()
+
+	query := "SELECT cab.id, cab.number, cab.size_id, cab.specifications " +
+		"FROM Cabinets cab " +
+		"INNER JOIN Companies comp ON cab.company_id = comp.id " +
+		"WHERE company_name = '" + companyName + "'"
+
+	selectResult, err := DBQueryMultiRow(db, query)
+
+	if err != nil {
+		return []Cabinet{}, errors.New("Cabinets not found!")
+	}
+
+	var answer []Cabinet
+
+	for selectResult.Next() {
+
+		var id, sizeId int
+		var number, specification string
+
+		selectResult.Scan(&id, &number, &sizeId, &specification)
+
+		answer = append(answer, Cabinet{
+			Id:       strconv.Itoa(id),
+			Number:   number,
+			Size:     strconv.Itoa(sizeId),
+			Property: specification,
+			Company:  companyName,
+		})
+
+		fmt.Println(strconv.Itoa(id), number, strconv.Itoa(sizeId), specification)
+	}
+
+	selectResult.Close()
+
+	return answer, nil
 }
 
 func GetCabinetInfoById(cabinetId string) (Cabinet, error) {
